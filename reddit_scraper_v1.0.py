d734e564f56a95f86c9b35170a3548cbb0ad9c38
benryan03 <50218096+benryan03@users.noreply.github.com>
2020-02-15 10:49:28 -0800

    v1.1 update

--- reddit_scraper_v1.0.py
+++ reddit_scraper_v1.0.py
@@ -1,20 +1,27 @@
-#reddit_scraper_v1.0
+#reddit_scraper_v1.1
 print("Reddit scraper")
 
 import praw
 import wget
 import os
 
-reddit = praw.Reddit('main_account', user_agent='main_account user agent')
+reddit = praw.Reddit('main_account', user_agent='main_account user agent') #this logs into a reddit account specified in praw.ini
 
-subreddit = reddit.subreddit('pics')
-new_subreddit = subreddit.new()
+subreddit_input = input("Enter subreddit: ")
+image_number = int(input("Enter the number of submissions to parse (limit 1000): "))
 
-for submission in subreddit.new(limit=3):  #hard limit is 1000
+subreddit = reddit.subreddit(subreddit_input)
+
+for submission in subreddit.new(limit=image_number):
 
     print(submission.title, submission.url)
-    file_name = wget.download(submission.url,)    
-    os.rename(file_name, "downloads/" + submission.title + " .jpg") 
-    print("\n")
+    file_extension = submission.url[len(submission.url) -4:]
+    print("file_extension: " + file_extension)
+    if file_extension == ".jpg" or file_extension == "png" or file_extension == ".bmp" or file_extension == "jpeg": #if url is a direct image link
+        file_name = wget.download(submission.url,)    
+        os.rename(file_name, "downloads/" + submission.title + " .jpg") 
+        print("\n")
+    else:
+        print("Submission is not a direct image link.")
 
 print("DEBUG: end")
